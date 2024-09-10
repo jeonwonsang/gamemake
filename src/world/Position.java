91522874844ea6f7db22a4564a1c165baf17e19d
jeonwonsang <94jws@naver.com>
2024-09-11 00:43:31 +0900

    Add files via upload

---
+++ src/world/Position.java
@@ -0,0 +1,10 @@
+package world;
+
+public class Position extends world{
+	
+	public void position() {
+		System.out.println("=======================");
+		System.out.println("          현위치 : (" + x + " / " + y + ")");
+		System.out.println("=======================");
+	}
+}
